b83a3611d5ead829537a22f151159ea85d1a999f
BaoTH799 <baoth799@gmail.com>
2022-09-18 21:58:35 +0700

    add server

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+const http = require('http')
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
-    await sleep(5000);
-  }
+  // while(true) {
+  //   console.log('Containers rule!');
+  //   await sleep(5000);
+  // }
+  const server = http.createServer((req, res) => {
+  console.log(req.url)
+  res.end('Udacity !!!!!')
+  })
+  server.listen(80)
 }
 
 main();
